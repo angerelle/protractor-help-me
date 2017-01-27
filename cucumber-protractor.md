a522e780c00a130b444b61db42c4480c84c820f5
Angela Relle <angela.relle@googlemail.com>
2017-01-27 18:13:29 +0000

    Working version using cut and paste protractor config

---
+++ cucumber-protractor.md
@@ -0,0 +1,24 @@
+# Protractor and Cucumber
+
+To get working on iMac
+
+## Protractor
+
+* Installed latest version of Node and NPM (had to completely remove 
+previous version) - this meant that I could get the webdriver updated
+
+* Updated Java, so that the webdriver would actually run
+
+* Started the web server, `npm start`
+
+* Got the Angular seed project Protractor tests working
+
+## Cucumber JS
+
+* Installed an older version of Cucumber JS so that the 
+protractor-cucumber-framework would install `npm install cucumber@^0.9.1`
+
+* Copied protractor config from <http://fizzylogic.nl/2016/10/14/How-to-write-ATDD-tests-with-cucumber-js-protractor-and-typescript/>
+
+
+
--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,25 +1,55 @@
 //jshint strict: false
-exports.config = {
+/*exports.config = {
 
-  allScriptsTimeout: 11000,
+    allScriptsTimeout: 11000,
 
-  specs: [
-    '*.js'
+    specs: [
+      '*.js'
   ],
 
-  capabilities: {
-    'browserName': 'chrome'
+    capabilities: {
+      'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/',
+    baseUrl: 'http://localhost:8000/',
 
-  framework: 'jasmine',
+    framework: 'jasmine',
 
-  jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    jasmineNodeOpts: {
+      defaultTimeoutInterval: 30000
   },
 
   directConnect: true
 
 
 };
+*/
+
+exports.config = {
+    baseUrl: 'http://localhost:8000',
+    // Specify the patterns for test files
+    // to include in the test run
+    specs: [
+        '../features/*.feature'
+    ],
+    // Use this to exclude files from the test run.
+    // In this case it's empty since we want all files
+    // that are mentioned in the specs.
+    exclude: [],
+    // Use cucumber for the tests
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    // Contains additional settings for cucumber-js
+    cucumberOpts: {
+    },
+    // These capabilities tell protractor about the browser
+    // it should use for the tests. In this case chrome.
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+        }
+    },
+    // This setting tells protractor to wait for all apps
+    // to load on the page instead of just the first.
+    useAllAngular2AppRoots: true
+}
